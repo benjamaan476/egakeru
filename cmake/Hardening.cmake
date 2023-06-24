591541a0120494069953012e812e17bb3a9f8432
Template Janitor <template.janitor@example.com>
2023-06-24 11:05:29 +0000

    Cleanup template and initialize repository

--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
 
 macro(
-  myproject_enable_hardening
+  egakeru_enable_hardening
   target
   global
   ubsan_minimal_runtime)
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <egakeru/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
