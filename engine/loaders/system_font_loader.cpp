6d6e886415b1f6e1b0241704f49a775ad0e88297
ben <ben.crabbe@hotmail.co.uk>
2025-01-26 18:07:20 +0000

    Fixed font and audio leak

--- engine/loaders/system_font_loader.cpp
+++ engine/loaders/system_font_loader.cpp
@@ -3,151 +3,148 @@
 
 namespace egkr
 {
-	system_font_loader::unique_ptr system_font_loader::create(const loader_properties& properties)
+    system_font_loader::unique_ptr system_font_loader::create(const loader_properties& properties) { return std::make_unique<system_font_loader>(properties); }
+
+    system_font_loader::system_font_loader(const loader_properties& properties): resource_loader(resource::type::system_font, properties) { }
+
+    resource::shared_ptr system_font_loader::load(const std::string& name, void* /*params*/)
+    {
+	auto base_path = get_base_path();
+
+	std::array<supported_system_font_file_type, 2> filetypes{supported_system_font_file_type{.extension = ".esf", .type = system_font_file_type::esf, .is_binary = true},
+	    supported_system_font_file_type{.extension = ".fontcfg", .type = system_font_file_type::font_config, .is_binary = false}};
+
+	std::string filename;
+	bool found{};
+	supported_system_font_file_type filetype{};
+	for (const auto& extension : filetypes)
 	{
-		return std::make_unique<system_font_loader>(properties);
+	    filename = std::format("{}/{}{}", base_path, name, extension.extension);
+
+	    if (filesystem::does_path_exist(filename))
+	    {
+		filetype = extension;
+		found = true;
+		break;
+	    }
 	}
 
-	system_font_loader::system_font_loader(const loader_properties& properties)
-		:resource_loader(resource::type::system_font, properties)
-	{}
-
-	resource::shared_ptr system_font_loader::load(const std::string& name, void* /*params*/)
+	if (!found)
 	{
-		auto base_path = get_base_path();
-
-		std::array<supported_system_font_file_type, 2> filetypes{ supported_system_font_file_type{".esf", system_font_file_type::esf, true},  supported_system_font_file_type{".fontcfg", system_font_file_type::font_config, false} };
-
-		std::string filename;
-		bool found{};
-		supported_system_font_file_type filetype{};
-		for (const auto& extension : filetypes)
-		{
-			filename = std::format("{}/{}{}", base_path, name, extension.extension);
-
-			if (filesystem::does_path_exist(filename))
-			{
-				filetype = extension;
-				found = true;
-				break;
-			}
-
-		}
-
-		if (!found)
-		{
-			LOG_ERROR("File not found: {}", filename);
-			return nullptr;
-		}
-
-
-		font::system_font_resource_data resource_data{};
-		auto file = filesystem::open(filename, file_mode::read, filetype.is_binary);
-
-		switch (filetype.type)
-		{
-		case system_font_file_type::font_config:
-		{
-			filename = std::format("{}/{}{}", base_path, name, ".ebf");
-
-			std::string ebf_file_name = filename;
-			resource_data = import_fontcfg_file(file, ebf_file_name);
-		} break;
-		case system_font_file_type::esf:
-		{
-			resource_data = read_esf_file(file);
-		} break;
-		case system_font_file_type::not_found:
-		default:
-		{
-			LOG_FATAL("Shouldn't have got here");
-		}
-		}
-
-		resource::properties properties{ .type = resource::type::system_font, .name = name.data(), .full_path = filename, };
-		properties.data = new font::system_font_resource_data();
-		*((font::system_font_resource_data*)(properties.data)) = resource_data;
-		return resource::create(properties);
+	    LOG_ERROR("File not found: {}", filename);
+	    return nullptr;
 	}
 
-	bool system_font_loader::unload(const resource::shared_ptr& resource)
+
+	font::system_font_resource_data resource_data{};
+	auto file = filesystem::open(filename, file_mode::read, filetype.is_binary);
+
+	switch (filetype.type)
 	{
-		if (resource->data)
-		{
-			free(resource->data);
-			resource->data = nullptr;
-		}
-		return true;
+	case system_font_file_type::font_config:
+	{
+	    filename = std::format("{}/{}{}", base_path, name, ".ebf");
+
+	    std::string ebf_file_name = filename;
+	    resource_data = import_fontcfg_file(file, ebf_file_name);
+	}
+	break;
+	case system_font_file_type::esf:
+	{
+	    resource_data = read_esf_file(file);
+	}
+	break;
+	case system_font_file_type::not_found:
+	default:
+	{
+	    LOG_FATAL("Shouldn't have got here");
+	}
 	}
 
-	font::system_font_resource_data system_font_loader::import_fontcfg_file(file_handle& handle, std::string_view esf_filename)
+	resource::properties properties{
+	    .type = resource::type::system_font,
+	    .name = name.data(),
+	    .full_path = filename,
+	};
+	properties.data = new font::system_font_resource_data();
+	*((font::system_font_resource_data*)(properties.data)) = resource_data;
+	return resource::create(properties);
+    }
+
+    bool system_font_loader::unload(const resource::shared_ptr& resource)
+    {
+	if (resource->data)
 	{
-		font::system_font_resource_data data{};
-		uint32_t line_number{};
-		egkr::vector<uint8_t> line{ '#' };
+	    free(resource->data);
+	    resource->data = nullptr;
+	}
+	return true;
+    }
 
-		for (; !line.empty(); line = filesystem::read_line(handle, 511), ++line_number)
-		{
-			std::string line_string{ line.begin(), line.end() };
-			trim(line_string);
+    font::system_font_resource_data system_font_loader::import_fontcfg_file(file_handle& handle, std::string_view esf_filename)
+    {
+	font::system_font_resource_data data{};
+	uint32_t line_number{};
+	egkr::vector<uint8_t> line{'#'};
 
-			if (line[0] == '#' || line[0] == '\0')
-			{
-				continue;
-			}
+	for (; !line.empty(); line = filesystem::read_line(handle, 511), ++line_number)
+	{
+	    std::string line_string{line.begin(), line.end()};
+	    trim(line_string);
 
-			auto split_index = line_string.find_first_of('=');
-			if (split_index == std::string::npos)
-			{
-				LOG_WARN("Potential formatting issue found in file {}: '=' token not found on line number {}.", handle.filepath.data(), line_number);
-				continue;
-			}
+	    if (line[0] == '#' || line[0] == '\0')
+	    {
+		continue;
+	    }
 
-			auto variable_name = line_string.substr(0, split_index);
-			trim(variable_name);
+	    auto split_index = line_string.find_first_of('=');
+	    if (split_index == std::string::npos)
+	    {
+		LOG_WARN("Potential formatting issue found in file {}: '=' token not found on line number {}.", handle.filepath.data(), line_number);
+		continue;
+	    }
 
-			auto value = line_string.substr(split_index + 1);
-			trim(value);
+	    auto variable_name = line_string.substr(0, split_index);
+	    trim(variable_name);
 
-			if (variable_name == "version")
-			{
+	    auto value = line_string.substr(split_index + 1);
+	    trim(value);
 
-			}
-			else if (variable_name == "file")
-			{
-				auto font_file = value;
-				auto font_resource = resource_system::load(font_file, resource::type::binary, nullptr);
-				binary_resource_properties* font_data = (binary_resource_properties*)font_resource->data;
-				data.binary_size = font_data->data.size();
-				data.font_binary = malloc(data.binary_size);
-				std::memcpy(data.font_binary, font_data->data.data(), data.binary_size);
-				resource_system::unload(font_resource);
-			}
-			else if(variable_name == "face")
-			{
-				font::system_font_face new_face{};
-				new_face.name = value;
-				data.fonts.push_back(new_face);
-			}
-		}
-
-		if (!data.font_binary || data.fonts.empty())
-		{
-			LOG_ERROR("Bad fontcfg file, {}. Required information missing", handle.filepath.data());
-			return data;
-		}
-
-		return write_esf_file(esf_filename, data);
+	    if (variable_name == "version")
+	    {
+	    }
+	    else if (variable_name == "file")
+	    {
+		auto font_file = value;
+		auto font_resource = resource_system::load(font_file, resource::type::binary, nullptr);
+		binary_resource_properties* font_data = (binary_resource_properties*)font_resource->data;
+		data.binary_size = font_data->data.size();
+		data.font_binary = malloc(data.binary_size);
+		std::memcpy(data.font_binary, font_data->data.data(), data.binary_size);
+		resource_system::unload(font_resource);
+	    }
+	    else if (variable_name == "face")
+	    {
+		font::system_font_face new_face{};
+		new_face.name = value;
+		data.fonts.push_back(new_face);
+	    }
 	}
 
-	font::system_font_resource_data system_font_loader::read_esf_file(file_handle& /*handle*/)
+	if (!data.font_binary || data.fonts.empty())
 	{
-		return font::system_font_resource_data();
+	    LOG_ERROR("Bad fontcfg file, {}. Required information missing", handle.filepath.data());
+	    return data;
 	}
 
-	font::system_font_resource_data system_font_loader::write_esf_file(std::string_view /*path*/, const font::system_font_resource_data& data)
-	{
-		//TODO
-		return data;
-	}
+	return write_esf_file(esf_filename, data);
+    }
+
+    font::system_font_resource_data system_font_loader::read_esf_file(file_handle& /*handle*/) { return {}; }
+
+    font::system_font_resource_data system_font_loader::write_esf_file(std::string_view /*path*/, const font::system_font_resource_data& data)
+    {
+	//TODO
+	return data;
+    }
 }
--- engine/plugins/audio/audio_loader.cpp
+++ engine/plugins/audio/audio_loader.cpp
@@ -10,175 +10,173 @@
 
 namespace egkr
 {
-	struct audio_file_internal
-	{
-		stb_vorbis* vorbis{};
-		//mp3dec_file_info_t mp3_info{};
-		int16_t* pcm{};
-		uint64_t pcm_size{};
-	};
+    struct audio_file_internal
+    {
+	stb_vorbis* vorbis{};
+	//mp3dec_file_info_t mp3_info{};
+	int16_t* pcm{};
+	uint64_t pcm_size{};
+    };
 
-	uint64_t audio::file::load_samples(uint32_t chunk_size, uint32_t /*count*/)
+    uint64_t audio::file::load_samples(uint32_t chunk_size, uint32_t /*count*/)
+    {
+	if (internal_data->vorbis)
 	{
-		if (internal_data->vorbis)
+	    int64_t samples = stb_vorbis_get_samples_short_interleaved(internal_data->vorbis, channels, internal_data->pcm, (int32_t)chunk_size);
+	    return (uint64_t)(samples * channels);
+	}
+	//else if (internal_data->mp3_info.buffer)
+	//{
+	//return min(total_samples_left, chunk_size);
+	//}
+
+	return invalid_64u_id;
+    }
+
+    void* audio::file::stream_buffer_data()
+    {
+	if (internal_data->vorbis)
+	{
+	    return internal_data->pcm;
+	}
+	//else if (internal_data->mp3_info.buffer)
+	//{
+	//uint64_t pos = internal_data->mp3_info.samples - total_samples_left;
+	//return internal_data->mp3_info.buffer + pos;
+	//}
+	else
+	{
+	    LOG_ERROR("Error streaming audio dta: unknown type");
+	    return nullptr;
+	}
+    }
+
+    void audio::file::rewind()
+    {
+	if (internal_data->vorbis)
+	{
+	    stb_vorbis_seek_start(internal_data->vorbis);
+	    total_samples_left = stb_vorbis_stream_length_in_samples(internal_data->vorbis) * (uint32_t)channels;
+	}
+	//else if (internal_data->mp3_info.buffer)
+	//{
+	//total_samples_left = internal_data->mp3_info.samples;
+	//}
+	else
+	{
+	    LOG_ERROR("Error rewinding audio file: unknown type");
+	}
+    }
+
+    audio_loader::unique_ptr audio_loader::create(const loader_properties& properties) { return std::make_unique<audio_loader>(properties); }
+
+    audio_loader::audio_loader(const loader_properties& properties): resource_loader{resource::type::audio, properties}
+    {
+	//mp3dec_init(&decoder);
+    }
+
+    resource::shared_ptr audio_loader::load(const std::string& name, void* params)
+    {
+	auto* parameters = std::bit_cast<audio_resource_loader_params*>(params);
+
+	auto base_path = get_base_path();
+	const std::string filename = std::format("{}/{}", base_path, name);
+
+	audio::file* resource_data = new audio::file();
+	resource_data->internal_data = new audio_file_internal();
+	resource_data->audio_type = parameters->type;
+	if (name.contains(".ogg"))
+	{
+	    LOG_TRACE("Processing OGG file");
+	    int32_t ogg_error{};
+
+	    resource_data->internal_data->vorbis = stb_vorbis_open_filename(filename.c_str(), &ogg_error, nullptr);
+	    if (!resource_data->internal_data->vorbis)
+	    {
+		LOG_ERROR("Failed to load vorbis file: {}", ogg_error);
+		return nullptr;
+	    }
+	    stb_vorbis_info info = stb_vorbis_get_info(resource_data->internal_data->vorbis);
+	    resource_data->channels = info.channels;
+	    resource_data->sample_rate = info.sample_rate;
+	    resource_data->total_samples_left = stb_vorbis_stream_length_in_samples(resource_data->internal_data->vorbis);
+
+	    if (resource_data->audio_type == audio::type::music_stream)
+	    {
+		const uint64_t buffer_lenght = parameters->chunk_size * sizeof(int16_t);
+		resource_data->internal_data->pcm = (int16_t*)malloc(buffer_lenght);
+		resource_data->internal_data->pcm_size = buffer_lenght;
+	    }
+	    else
+	    {
+		uint64_t length_samples = stb_vorbis_stream_length_in_samples(resource_data->internal_data->vorbis) * (uint32_t)info.channels;
+		const uint64_t buffer_lenght = length_samples * sizeof(int16_t);
+
+		resource_data->internal_data->pcm = (int16_t*)malloc(buffer_lenght);
+		resource_data->internal_data->pcm_size = buffer_lenght;
+		const int32_t read_samples = stb_vorbis_get_samples_short_interleaved(resource_data->internal_data->vorbis, info.channels, resource_data->internal_data->pcm, (int32_t)length_samples);
+		if ((uint64_t)read_samples != length_samples)
 		{
-			int64_t samples = stb_vorbis_get_samples_short_interleaved(internal_data->vorbis, channels, internal_data->pcm, (int32_t)chunk_size);
-			return (uint64_t)(samples * channels);
+		    LOG_WARN("Read/length mismatch while reading ogg file.");
 		}
-		//else if (internal_data->mp3_info.buffer)
+
+		length_samples += (length_samples % 4);
+		resource_data->total_samples_left = length_samples;
+	    }
+	}
+	else if (name.contains(".mp3"))
+	{
+	    LOG_TRACE("Processing mp3 file");
+	    //mp3dec_load(&decoder, buff, &resource_data->internal_data->mp3_info, 0, nullptr);
+	    //mp3dec_file_info_t& info = resource_data->internal_data->mp3_info;
+	    //resource_data->channels = info.channels;
+	    //resource_data->sample_rate = info.hz;
+	    //resource_data->total_samples_left = info.samples;
+	}
+	else
+	{
+	    LOG_ERROR("Unsupported audio file type");
+	    return nullptr;
+	}
+
+	resource::properties audio_properties{};
+	audio_properties.type = resource::type::audio;
+	audio_properties.name = name;
+	audio_properties.full_path = filename;
+
+	audio_properties.data = new (audio::file);
+	*(audio::file*)audio_properties.data = *resource_data;
+
+	return resource::create(audio_properties);
+    }
+
+    bool audio_loader::unload(const resource::shared_ptr& resource)
+    {
+	if (resource->data)
+	{
+	    auto* file = (audio::file*)resource->data;
+
+	    if (file->internal_data != nullptr)
+	    {
+		if (file->internal_data->vorbis != nullptr)
+		{
+		    stb_vorbis_close(file->internal_data->vorbis);
+		}
+		//else if (file->internal_data->mp3_info.buffer)
 		//{
-			//return min(total_samples_left, chunk_size);
+		//
 		//}
+		if (file->internal_data->pcm)
+		{
+		    delete file->internal_data->pcm;
+		    file->internal_data->pcm = nullptr;
+		}
 
-		return invalid_64u_id;
+		delete file->internal_data;
+	    }
+	    delete (audio::file*)resource->data;
+	    resource->data = nullptr;
 	}
-
-	void* audio::file::stream_buffer_data()
-	{
-		if (internal_data->vorbis)
-		{
-			return internal_data->pcm;
-		}
-		//else if (internal_data->mp3_info.buffer)
-		//{
-			//uint64_t pos = internal_data->mp3_info.samples - total_samples_left;
-			//return internal_data->mp3_info.buffer + pos;
-		//}
-		else
-		{
-			LOG_ERROR("Error streaming audio dta: unknown type");
-			return nullptr;
-		}
-	}
-
-	void audio::file::rewind()
-	{
-		if (internal_data->vorbis)
-		{
-			stb_vorbis_seek_start(internal_data->vorbis);
-			total_samples_left = stb_vorbis_stream_length_in_samples(internal_data->vorbis) * (uint32_t)channels;
-		}
-		//else if (internal_data->mp3_info.buffer)
-		//{
-			//total_samples_left = internal_data->mp3_info.samples;
-		//}
-		else
-		{
-			LOG_ERROR("Error rewinding audio file: unknown type");
-		}
-	}
-
-	audio_loader::unique_ptr audio_loader::create(const loader_properties& properties)
-	{
-		return std::make_unique<audio_loader>(properties);
-	}
-
-	audio_loader::audio_loader(const loader_properties& properties)
-		: resource_loader{resource::type::audio, properties}
-	{
-		//mp3dec_init(&decoder);
-	}
-
-	resource::shared_ptr audio_loader::load(const std::string& name, void* params)
-	{
-		auto* parameters = std::bit_cast<audio_resource_loader_params*>(params);
-
-		auto base_path = get_base_path();
-		const std::string filename = std::format("{}/{}", base_path, name);
-
-		audio::file* resource_data = new audio::file();
-		resource_data->internal_data = new audio_file_internal();
-		resource_data->audio_type = parameters->type;
-		if (name.contains(".ogg"))
-		{
-			LOG_TRACE("Processing OGG file");
-			int32_t ogg_error{};
-
-			resource_data->internal_data->vorbis = stb_vorbis_open_filename(filename.c_str(), &ogg_error, nullptr);
-			if (!resource_data->internal_data->vorbis)
-			{
-				LOG_ERROR("Failed to load vorbis file: {}", ogg_error);
-				return nullptr;
-			}
-			stb_vorbis_info info = stb_vorbis_get_info(resource_data->internal_data->vorbis);
-			resource_data->channels = info.channels;
-			resource_data->sample_rate = info.sample_rate;
-			resource_data->total_samples_left = stb_vorbis_stream_length_in_samples(resource_data->internal_data->vorbis);
-
-			if (resource_data->audio_type == audio::type::music_stream)
-			{
-				const uint64_t buffer_lenght = parameters->chunk_size * sizeof(int16_t);
-				resource_data->internal_data->pcm = (int16_t*)malloc(buffer_lenght);
-				resource_data->internal_data->pcm_size = buffer_lenght;
-			}
-			else
-			{
-				uint64_t length_samples = stb_vorbis_stream_length_in_samples(resource_data->internal_data->vorbis) * (uint32_t)info.channels;
-				const uint64_t buffer_lenght = length_samples * sizeof(int16_t);
-
-				resource_data->internal_data->pcm = (int16_t*)malloc(buffer_lenght);
-				resource_data->internal_data->pcm_size = buffer_lenght;
-				const int32_t read_samples = stb_vorbis_get_samples_short_interleaved(resource_data->internal_data->vorbis, info.channels, resource_data->internal_data->pcm, (int32_t)length_samples);
-				if ((uint64_t)read_samples != length_samples)
-				{
-					LOG_WARN("Read/length mismatch while reading ogg file.");
-				}
-
-				length_samples += (length_samples % 4);
-				resource_data->total_samples_left = length_samples;
-			}
-		}
-		else if (name.contains(".mp3"))
-		{
-			LOG_TRACE("Processing mp3 file");
-			//mp3dec_load(&decoder, buff, &resource_data->internal_data->mp3_info, 0, nullptr);
-			//mp3dec_file_info_t& info = resource_data->internal_data->mp3_info;
-			//resource_data->channels = info.channels;
-			//resource_data->sample_rate = info.hz;
-			//resource_data->total_samples_left = info.samples;
-		}
-		else
-		{
-			LOG_ERROR("Unsupported audio file type");
-			return nullptr;
-		}
-
-		resource::properties audio_properties{};
-		audio_properties.type = resource::type::audio;
-		audio_properties.name = name;
-		audio_properties.full_path = filename;
-
-		audio_properties.data = new(audio::file);
-		*(audio::file*)audio_properties.data = *resource_data;
-
-		return resource::create(audio_properties);
-	}
-
-	bool audio_loader::unload(const resource::shared_ptr& resource)
-	{
-		if (resource->data)
-		{
-			audio::file* file = (audio::file*)resource->data;
-
-			if (file->internal_data)
-			{
-				if (file->internal_data->vorbis)
-				{
-					stb_vorbis_close(file->internal_data->vorbis);
-				}
-				//else if (file->internal_data->mp3_info.buffer)
-				//{
-//
-				//}
-				if (file->internal_data->pcm)
-				{
-					delete file->internal_data->pcm;
-					file->internal_data->pcm = nullptr;
-				}
-
-				delete file->internal_data;
-			}
-		}
-			return true;
-	}
+	return true;
+    }
 }
